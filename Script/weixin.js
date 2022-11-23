253371fc983326673cc501377e43576b472a833b
lhsll <82965551+lhsll@users.noreply.github.com>
2022-11-24 07:41:49 +0800

    Add files via upload

---
+++ Script/weixin.js
@@ -0,0 +1,3 @@
+let app = JSON.parse($request.body);
+"advertisement_num":\d,"advertisement_info":\[.+\] "advertisement_num":0,"advertisement_info":[];
+$done({body:JSON.stringify(app)});
---
+++ Script/xiaohongshu.js
@@ -0,0 +1,5 @@
+let app = JSON.parse($request.body);
+items":\[.+\] response-body items":[];
+queries":\[.+\]
+queries":[]
+$done({body:JSON.stringify(app)});
